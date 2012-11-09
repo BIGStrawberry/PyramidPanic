af75036dd0b93dadbb3a0f49880850842420b47f
Wouter Dijkstra <wody.msn@hotmail.com>
2012-11-09 11:40:01 +0100

    StartScene, Buttons, Background

---
+++ PyramidPanic/PyramidPanic/PyramidPanic/Game/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace PyramidPanic
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (PyramidPanic game = new PyramidPanic())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
